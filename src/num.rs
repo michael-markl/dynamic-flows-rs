aec62d436dae9f47117c99f004235631b88e62c7
Michael Markl <marklmichael98@gmail.com>
2023-08-05 20:30:46 +0200

    Strengthen Num trait even more

--- src/num.rs
+++ src/num.rs
@@ -1,8 +1,19 @@
 use core::fmt::Debug;
-use num_traits::Num as num_traits_Num;
-use std::{fmt::Display, ops::Neg};
+use num_traits::{Num as num_traits_Num, NumAssignOps, Signed};
+use std::{fmt::Display, hash::Hash, ops::Neg};
 
-pub trait Num: num_traits_Num + Neg<Output = Self> + Copy + Ord + Debug + Display {
+pub trait Num:
+    num_traits_Num
+    + Neg<Output = Self>
+    + Signed
+    + Ord
+    + Copy
+    + Debug
+    + Display
+    + Hash
+    + NumAssignOps
+    + std::iter::Sum
+{
     const EXACT_ARITHMETIC: bool;
     const ZERO: Self;
     const ONE: Self;
@@ -10,3 +21,17 @@ pub trait Num: num_traits_Num + Neg<Output = Self> + Copy + Ord + Debug + Displa
     const INFINITY: Self;
     fn to_f64(self) -> f64;
 }
+
+pub trait Sum: for<'a> Iterator {
+    fn sum_iter<'a, T: Num + 'a>(self) -> T
+    where
+        Self: Iterator<Item = &'a T> + Sized,
+    {
+        self.fold(T::ZERO, |mut acc, x| {
+            acc += *x;
+            return acc;
+        })
+    }
+}
+
+impl<'a, T: Num + 'a, I: Iterator<Item = &'a T>> Sum for I {}
